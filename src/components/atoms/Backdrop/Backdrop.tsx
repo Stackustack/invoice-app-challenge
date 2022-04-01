e577b1b2e6ab8434bacb1b3cd6c794b610af552c
Michał Jung <stackustack@gmail.com>
2022-04-01 15:54:56 +0200

    updating backdrop atom

--- src/components/atoms/Backdrop/Backdrop.tsx
+++ src/components/atoms/Backdrop/Backdrop.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { ReactNode, useEffect } from "react";
 
 import s from "./Backdrop.module.scss";
 
-type Props = {};
+type Props = {
+  children?: ReactNode;
+};
 
-const Backdrop = (props: Props) => {
-  return <div className={s.backdrop} />;
+const Backdrop = ({ children }: Props) => {
+  useEffect(() => {
+    document.body.style.overflowY = "hidden";
+    return () => {
+      document.body.style.overflowY = "auto";
+    };
+  }, []);
+
+  return <div className={s.backdrop}>{children}</div>;
 };
 
 export default Backdrop;
